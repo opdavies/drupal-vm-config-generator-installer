1070946380b285e4a1dea2ceb1b332df3ae78a3a
Oliver Davies <oliver@oliverdavies.uk>
2016-03-10 15:32:17 +0000

    Set the default timezone

--- web/app.php
+++ web/app.php
@@ -4,6 +4,8 @@ use DrupalVmConfigGenerator\Installer\Application;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+date_default_timezone_set('Europe/London');
+
 $app = new Application();
 
 $app->run();
